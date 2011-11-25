36d245dfbcfb492ba4a9d220dc49790f27e6ce56
Hedgehog <hedgehogshiatus@gmail.com>
2011-11-25 15:39:41 +1100

    write ttmux.conf to root_dir (tmuxinator yaml path)

--- lib/tmuxinator/cli.rb
+++ lib/tmuxinator/cli.rb
@@ -142,7 +142,9 @@ module Tmuxinator
         config_path = "#{root_dir}#{project_name}.yml"
         config = Tmuxinator::ConfigWriter.new(config_path).render
         # writeout the tmux confi to ttmux.conf (ttmux is not a typo))
-        File.open(File.join(root_dir, 'ttmux.conf'), mode: 'w') {|io| io.write(config)}
+        File.new(File.join(root_dir, 'ttmux.conf'), 'w') do |io|
+          io.write(config)
+        end
         # replace current proccess by running compiled tmux config
         exec(config)
       end
